a766d8802c604d620dd60e5a2b6cade518b59c59
Serafin Quesada <quesada.serafin03@gmail.com>
2024-02-05 19:10:38 -0300

    last details

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,45 +1,46 @@
-"use client";
-import React, { useState } from "react";
-import GithubIcon from "../../../public/github-icon.svg";
-import LinkedinIcon from "../../../public/linkedin-icon.svg";
-import Link from "next/link";
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react'
+import GithubIcon from '../../../public/github-icon.svg'
+import LinkedinIcon from '../../../public/linkedin-icon.svg'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const EmailSection = () => {
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
+  const [emailSubmitted, setEmailSubmitted] = useState(false)
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
+    const { email, subject, message } = e.target
     const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
+      email: email.value,
+      subject: subject.value,
+      message: message.value,
+    }
+    const JSONdata = JSON.stringify(data)
+    const endpoint = '/api/send'
 
     // Form the request for sending data to the server.
     const options = {
       // The method is POST because we are sending data.
-      method: "POST",
+      method: 'POST',
       // Tell the server we're sending JSON.
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       // Body of the request is the JSON data we created above.
       body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-
-    const resData = await response.json();
-    console.log(resData);
-    
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
     }
-  };
+
+    const response = await fetch(endpoint, options)
+
+    const resData = await response.json()
+    console.log(resData)
+
+    if (response.status === 200) {
+      console.log('Message sent.')
+      setEmailSubmitted(true)
+    }
+  }
 
   return (
     <section id='contact' className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
@@ -52,10 +53,10 @@ const EmailSection = () => {
           abierta. Ya sea que tengas una pregunta o simplemente quieras saludar.
         </p>
         <div className='socials flex flex-row gap-2'>
-          <Link href='github.com'>
+          <Link target='_blanck' href='https://github.com/Seraquesada'>
             <Image src={GithubIcon} alt='Github Icon' />
           </Link>
-          <Link href='linkedin.com'>
+          <Link target='_blanck' href='https://www.linkedin.com/in/serafin-quesada/'>
             <Image src={LinkedinIcon} alt='Linkedin Icon' />
           </Link>
         </div>
@@ -67,7 +68,7 @@ const EmailSection = () => {
           <form className='flex flex-col' onSubmit={handleSubmit}>
             <div className='mb-6'>
               <label htmlFor='email' className='text-white block mb-2 text-sm font-medium'>
-                  Tu mail
+                Tu mail
               </label>
               <input
                 name='email'
@@ -75,7 +76,7 @@ const EmailSection = () => {
                 id='email'
                 required
                 className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                placeholder='jacob@google.com'
+                placeholder='serafin.quesada03@google.com'
               />
             </div>
             <div className='mb-6'>
@@ -88,7 +89,7 @@ const EmailSection = () => {
                 id='subject'
                 required
                 className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                placeholder='Just saying hi'
+                placeholder='Tema a hablar'
               />
             </div>
             <div className='mb-6'>
@@ -99,7 +100,7 @@ const EmailSection = () => {
                 name='message'
                 id='message'
                 className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                placeholder="Let's talk about..."
+                placeholder='Mensaje'
               />
             </div>
             <button
@@ -113,6 +114,6 @@ const EmailSection = () => {
       </div>
     </section>
   )
-};
+}
 
-export default EmailSection;
+export default EmailSection
